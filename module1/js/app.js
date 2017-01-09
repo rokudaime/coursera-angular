0610bf482341a41d1d8c3962502fac0ae58383a3
Sergey <rockudaime@user.noreply.github.com>
2017-01-09 22:22:46 +0200

    Angular app for module 1 of the course

---
+++ module1/js/app.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+  angular.module("LunchCheck", [])
+  .controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController ($scope) {
+    $scope.foodItems = "";
+    $scope.message = "This is my message";
+    $scope.calculateIfTooMuch = function () {
+      if ($scope.foodItems) {
+        var arr = $scope.foodItems.split(",");
+        var len = arr.length;
+        console.log(len);
+        if (len <= 3) {
+          $scope.message = "Enjoy!";
+            $scope.messageStatus = "success";
+        } else {
+          $scope.message = "Too much!";
+          $scope.messageStatus = "success";
+        }
+      } else {
+        $scope.message = "Please enter data first";
+        $scope.messageStatus = "error";
+      }
+
+      $scope.updateStatus = function() {
+        $scope.messageStatus = "";
+      }
+
+
+    }
+
+  };
+})() ;
